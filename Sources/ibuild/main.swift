6da9ff34519bbf39a26ae085402768773ef71f17
Ian McDowell <me@ianmcdowell.net>
2017-12-29 21:39:25 -0800

    Added clean action

--- Sources/ibuild/main.swift
+++ Sources/ibuild/main.swift
@@ -10,7 +10,7 @@ enum IBuildError: LocalizedError {
     }
 }
 
-do {
+    do {
     // Get package root (where the current package to build is)
     guard let packageRootEnv = ProcessInfo.processInfo.environment["PACKAGE_ROOT"] else {
         throw IBuildError.packageRootNotFound
@@ -20,38 +20,58 @@ do {
     let sourceRoot = filesRoot.appendingPathComponent("checkout")
     let buildRoot = filesRoot.appendingPathComponent("build")
 
-    // Load the package in that root
-    let package = try Package.inProject(fileURL: packageRoot)
-
-    // Get the project source map for this package
-    let projectSourceMap = ProjectSourceMap.inRoot(filesRoot)
-
-    // Save the root package into the map
-    projectSourceMap.locations[package.url] = packageRoot.path
-
-    // Get its dependencies
-    let dependencies = try DependencyDownloader.downloadDependencies(ofPackage: package, intoSourceRoot: sourceRoot, projectSourceMap: projectSourceMap)
-
-    // Sort dependencies into build order
-    let sorted = DependencySorter.buildOrder(forBuilding: dependencies)
-
-    if !sorted.isEmpty {
-        print("Building dependencies:")
-        for dependency in sorted {
-            print("\(dependency.name) - \(dependency.url)")
-        }
-        for dependency in sorted {
-            let builder = try Builder.forPackage(dependency, projectSourceMap: projectSourceMap, buildRoot: buildRoot)
-            try builder.build()
-        }
+    // Parse cmd line
+    let argc = CommandLine.arguments.count
+    let action: String
+    if argc > 1 {
+        action = CommandLine.arguments[1]
+    } else {
+        action = "build"
     }
 
-    // Download library
-    try DependencyDownloader.downloadLibrary(package.library, intoSourceRoot: sourceRoot, projectSourceMap: projectSourceMap)
+    switch action {
+    case "build": 
 
-    // Build library
-    let builder = try Builder.forPackage(package, projectSourceMap: projectSourceMap, buildRoot: buildRoot)
-    try builder.build()
+        // Load the package in that root
+        let package = try Package.inProject(fileURL: packageRoot)
+
+        // Get the project source map for this package
+        let projectSourceMap = ProjectSourceMap.inRoot(filesRoot)
+
+        // Save the root package into the map
+        projectSourceMap.locations[package.url] = packageRoot.path
+
+        // Get its dependencies
+        let dependencies = try DependencyDownloader.downloadDependencies(ofPackage: package, intoSourceRoot: sourceRoot, projectSourceMap: projectSourceMap)
+
+        // Sort dependencies into build order
+        let sorted = DependencySorter.buildOrder(forBuilding: dependencies)
+
+        if !sorted.isEmpty {
+            print("Building dependencies:")
+            for dependency in sorted {
+                print("\(dependency.name) - \(dependency.url)")
+            }
+            for dependency in sorted {
+                let builder = try Builder.forPackage(dependency, projectSourceMap: projectSourceMap, buildRoot: buildRoot)
+                try builder.build()
+            }
+        }
+
+        // Download library
+        try DependencyDownloader.downloadLibrary(package.library, intoSourceRoot: sourceRoot, projectSourceMap: projectSourceMap)
+
+        // Build library
+        let builder = try Builder.forPackage(package, projectSourceMap: projectSourceMap, buildRoot: buildRoot)
+        try builder.build()
+
+    case "clean":
+        try FileManager.default.removeItem(at: buildRoot)
+        print("Successfully cleaned project.")
+    default:
+        print("Invalid action: \(action). Options are: \"build\", \"clean\".")
+    }
+
 } catch {
     print(error.localizedDescription)
 }
